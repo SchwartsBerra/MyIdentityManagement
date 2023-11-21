1208c4d06317215b9b946cc1bc868ba8b9c38934
SchwartsBerra <101537039+SchwartsBerra@users.noreply.github.com>
2023-11-21 07:58:50 +0200

    Update MyIdentityManagement.sol

--- MyIdentityManagement.sol
+++ MyIdentityManagement.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract IdentityManagement {
+    address public admin;
+    mapping(address => bool) public isRegistered;
+    mapping(address => string) public identities;
+
+    event IdentityRegistered(address indexed user, string identity);
+    event IdentityUpdated(address indexed user, string identity);
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can call this function");
+        _;
+    }
+
+    modifier onlyRegistered() {
+        require(isRegistered[msg.sender], "User is not registered");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+
+}
